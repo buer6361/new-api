c88ae11c085e7c460d1eaaae41092dfac445f90f
不二 <buer@Buers-Air.local>
2026-05-16 02:42:04 +0900

    chore: replace New API branding with custom brand in classic theme footer and about page

--- web/classic/src/components/layout/Footer.jsx
+++ web/classic/src/components/layout/Footer.jsx
@@ -197,16 +197,8 @@ const FooterBar = () => {
 
           <div className='text-sm'>
             <span className='!text-semi-color-text-1'>
-              {t('设计与开发由')}{' '}
+              {systemName}
             </span>
-            <a
-              href='https://github.com/QuantumNous/new-api'
-              target='_blank'
-              rel='noopener noreferrer'
-              className='!text-semi-color-primary font-medium'
-            >
-              New API
-            </a>
           </div>
         </div>
       </footer>
@@ -229,16 +221,8 @@ const FooterBar = () => {
             ></div>
             <div className='text-sm flex-shrink-0'>
               <span className='!text-semi-color-text-1'>
-                {t('设计与开发由')}{' '}
+                {systemName}
               </span>
-              <a
-                href='https://github.com/QuantumNous/new-api'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='!text-semi-color-primary font-medium'
-              >
-                New API
-              </a>
             </div>
           </div>
         </footer>
